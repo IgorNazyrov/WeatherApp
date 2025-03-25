6bf20114d02605892a47ed807bbcb1d3e788b246
Игорь Назыров <igornazirob@mail.ru>
2025-03-25 19:18:51 +0500

    idk

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,36 @@
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 import { default as rollUpUrl } from '@rollup/plugin-url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    rollUpUrl({
-      include: ['**/*.woff2', '**/*.woff', '**/*.ttf', '**/*.eot', '**/*.svg'],
-      limit: Infinity,
-      publicPath: '/',
-    }),
-  ],
-  css: {
-    // additionalData: `@import "${path.resolve(__dirname, 'src/styles/_mixins.scss')}";`  ,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
     },
-  },
+    plugins: [
+      react(),
+      svgr(),
+      rollUpUrl({
+        include: ['**/*.woff2', '**/*.woff', '**/*.ttf', '**/*.eot', '**/*.svg'],
+        limit: Infinity,
+        publicPath: '/',
+      }),
+    ],
+    css: {
+      // additionalData: `@import "${path.resolve(__dirname, 'src/styles/_mixins.scss')}";`  ,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
 });
